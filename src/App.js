2f6d6beef4db923d244e7b63763181e5ecf62285
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-11 22:59:53 +0000

    Created main components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
-       Hello World
+       <Header />
+       <Main />
+       <Footer />
     </div>
   );
 }
