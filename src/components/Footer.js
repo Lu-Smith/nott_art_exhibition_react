20828b864c3679f9b4852f9b2e2374438e91f6a9
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-11 23:52:29 +0000

    Updated Footer,js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Contact, Container, Logo, Motto } from './styles/Footer.styled';
+import LogoImg from '../images/logo.png';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Container>
+        <Logo src={LogoImg} alt='Luna Smith Art logo'/>
+        <Contact>Contact</Contact>
+        <Motto>everything already happened</Motto>
+    </Container>
   )
 }
 
---
+++ src/components/styles/Footer.styled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+width: 100vw;
+height: 15vh;
+display: flex;
+align-items: center;
+justify-content: space-evenly;
+background-color: black;
+`
+export const Logo = styled.img`
+width: 300px;
+height: 50px;
+`
+export const Contact = styled.h1`
+font-size: 40px;
+color: #FA0421;
+`
+export const Motto = styled.h4`
+font-size: 20px;
+color: white;
+font-style: italic;
+`
