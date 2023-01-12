14ef938e94d07289d3034513c945619a748bde39
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-12 22:47:32 +0000

    Updated Control.styled.js

--- src/components/styles/Contol.styled.js
+++ src/components/styles/Contol.styled.js
@@ -5,9 +5,12 @@ export const Button = styled.button`
  color: #04E0DB;
  padding: 15px 20px;
  margin: 30px 0 20px 0;
+ box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.5);
+ border: none;
+ background-color: whitesmoke;
 `
 export const Scroll = styled.div`
-font-size: 30px;
+font-size: 25px;
 color: #04E0DB;
 margin-bottom: 10px;
 `
