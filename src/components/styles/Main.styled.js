7b59bce8aa952814ef1c2e74edb957e8de11ffbc
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-12 20:00:38 +0000

    Updated Main.styled.js

--- src/components/styles/Main.styled.js
+++ src/components/styles/Main.styled.js
@@ -4,15 +4,14 @@ export const Container = styled.div`
 width: 100vw;
 height: 70vh;
 display: flex;
+flex-direction: column;
 align-items: center;
 justify-content: space-evenly;
 background: rgb(250, 250, 250);
 `
 export const Title = styled.h2`
 font-size: 30px;
-color: #FA0421;
 `
 export const Description = styled.h3`
 font-size: 20px;
-color: black;
 `
