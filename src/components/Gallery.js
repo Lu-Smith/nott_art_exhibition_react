ea5b84491ca7d353dfb598178d2f9c8a98bd5c98
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-12 22:34:44 +0000

    Updated Gallery.js

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Nott1 from '../images/The Warrior of Shattered Tears by Luna Smith.jpg';
+import { Image, Title } from './styles/Gallery.styled';
+import { Container } from './styles/Main.styled';
 
 const Gallery = () => {
   return (
-    <div>Gallery</div>
+    <Container>
+        <Image src={Nott1} alt='Nott1' />
+        <Title> Nott1</Title>
+    </Container>
   )
 }
 
---
+++ src/components/styles/Gallery.styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+width: 100vw;
+height: 70vh;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: space-evenly;
+background: rgb(250, 250, 250);
+`
+export const Image = styled.img`
+width: 50%;
+`
+export const Title = styled.h3`
+font-size: 20px;
+`
