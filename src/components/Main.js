16d5c2eac327a7e8727a22610f595e59d6ba85a5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-12 19:59:02 +0000

    Created Main.js components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Control from './Control'
+import Gallery from './Gallery'
 import { Container, Description, Title } from './styles/Main.styled'
 
 const Main = () => {
   return (
     <Container>
         <Title>Search though time</Title>
+        <Gallery />
+        <Control />
+
         <Description>We are leaving in the endless loop</Description>
     </Container>
   )
