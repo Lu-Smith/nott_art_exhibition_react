9b51e3aeef69fd154511672d9237f1e8f8be4a1f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-12 23:02:49 +0000

    Created imagesList.js

---
+++ src/helpers/imagesList.js
@@ -0,0 +1,39 @@
+const content = [
+    {
+        id: 1,
+        title: "The Warrior of Shattered Tears",
+        alt: "The Warrior of Shattered Tears - an oil painitng from Nott collection by Luna Smith",
+        description: "A candle in the dark<br />Spark by spark lighting the corner of infinite space. There are invariably unreachable horizons",
+        src: "../images/The Warrior of Shattered Tears by Luna Smith.jpg",
+    },
+    {
+        id: 2,
+        title: "Awakening from the Sleep of Fear",
+        alt: "Awakening from the Sleep of Fear- an oil painitng from Nott collection by Luna Smith",
+        description: "A candle in the dark<br />Spark by spark lighting the corner of infinite space. There are invariably unreachable horizons",
+        src: "../images/Awakening from the Sleep of Fear by Luna Smith.jpg",
+    },
+    {
+        id: 3,
+        title: "The Battle of Endless Seas",
+        alt: "The Battle of Endless Seas - an oil painitng from Nott collection by Luna Smith",
+        description: "A candle in the dark<br />Spark by spark lighting the corner of infinite space. There are invariably unreachable horizons",
+        src: "../images/The Battle of Endless Seas by Luna Smith.jpg",
+    },
+    {
+        id: 4,
+        title: "The Soothing Wind of Longing",
+        alt: "The Soothing Wind of Longing - an oil painitng from Nott collection by Luna Smith",
+        description: "A candle in the dark<br />Spark by spark lighting the corner of infinite space. There are invariably unreachable horizons",
+        src: "../images/The Soothing Wind of Longing by Luna Smith.jpg",
+    },
+    {
+        id: 5,
+        title: "Dreams of a Future Self",
+        alt: "Dreams of a Future Self - an oil painitng from Nott collection by Luna Smith",
+        description: "A candle in the dark<br />Spark by spark lighting the corner of infinite space. There are invariably unreachable horizons",
+        src: "../images/Dreams of a Future Self by Luna Smith.jpg",
+    }
+]
+
+export default content
