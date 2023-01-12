b1f46a5bcc54dbdc38a18260a8aa6758d47aa004
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-12 00:08:20 +0000

    Updated styled components

--- src/components/styles/Footer.styled.js
+++ src/components/styles/Footer.styled.js
@@ -6,15 +6,15 @@ height: 15vh;
 display: flex;
 align-items: center;
 justify-content: space-evenly;
-background-color: black;
+background: linear-gradient(360deg, rgb(32, 38, 57) 11.4%, rgb(63, 76, 119) 70.2%);
 `
 export const Logo = styled.img`
 width: 300px;
 height: 50px;
 `
-export const Contact = styled.h1`
+export const Contact = styled.h2`
 font-size: 40px;
-color: #FA0421;
+color: #04E0DB;
 `
 export const Motto = styled.h4`
 font-size: 20px;
--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -6,7 +6,7 @@ height: 15vh;
 display: flex;
 align-items: center;
 justify-content: space-evenly;
-background-color: black;
+background: linear-gradient(180deg, rgb(32, 38, 57) 11.4%, rgb(63, 76, 119) 70.2%);
 `
 export const Logo = styled.img`
 width: 300px;
@@ -14,7 +14,7 @@ height: 50px;
 `
 export const Title = styled.h1`
 font-size: 60px;
-color: #FA0421;
+color: #04E0DB;
 `
 export const Motto = styled.h4`
 font-size: 20px;
