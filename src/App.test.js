cff9a23749302c351a1c58374cfb1bda8b88d6fe
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-13 07:13:21 +0000

    Updated App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -18,3 +18,14 @@ test('it should have a correct title', () => {
   const element = screen.getByText("Search though time");
   expect(element).toBeInTheDocument();
 });
+
+
+//footer
+test('it should have a correct footer', () => {
+  render(<App />);
+  const elementLink = screen.getByText("Buy art");
+  expect(elementLink).toBeInTheDocument();
+
+  const elementMotto = screen.getByText("the most important moment in time is now");
+  expect(elementMotto).toBeInTheDocument();
+});
