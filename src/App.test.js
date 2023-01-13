4cb2ad3646719db4471f583f817a4b6817cda1e9
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-13 07:09:54 +0000

    Updated App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// header 
+test('it should have a correct header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const elementTitle = screen.getByText("Nott");
+  expect(elementTitle).toBeInTheDocument();
+
+  const elementMotto = screen.getByText("connect the past, the present and the future");
+  expect(elementMotto).toBeInTheDocument();
+});
+
+//main
+
+test('it should have a correct title', () => {
+  render(<App />);
+  const element = screen.getByText("Search though time");
+  expect(element).toBeInTheDocument();
 });
