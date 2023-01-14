f2b02c1ac95426930ca8dd7355bc15d6c81b6091
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-14 23:20:53 +0000

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,32 @@ import Header from './components/Header';
 import Main from './components/Main';
 import { GalleryContainer, AppContainer } from './components/styles/App.styled';
 import content from './helpers/imagesList';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  dark: {
+    primaryColor: 'white',
+    primaryText: 'black',
+  },
+  light: {
+    primaryColor: 'black',
+    primaryText: 'white',
+  }
+}
 
 function App() {
   return (
-    <AppContainer>
-       <Header />
-       <GalleryContainer>
-          {content.map((image) => {
-            return <Main key={image.id} image={image} />
-          })}
-      </GalleryContainer>
-      <Footer />
-    </AppContainer>
+    <ThemeProvider theme={theme}>
+      <AppContainer>
+        <Header />
+        <GalleryContainer>
+            {content.map((image) => {
+              return <Main key={image.id} image={image} />
+            })}
+        </GalleryContainer>
+        <Footer />
+      </AppContainer>
+    </ThemeProvider>
   );
 }
 
