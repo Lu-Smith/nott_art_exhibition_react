4dbdfe782cfb182ef87944db570f7f2078fca37b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-17 15:38:58 +0000

    Updated Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,37 +2,42 @@ import React from 'react';
 import Gallery from './Gallery';
 import Description from './Description';
 import { Container, Title } from './styles/Main.styled';
-
-
+import { useSelector } from 'react-redux'
 
 const Main = (props) => {
-  
 
-  if (props.image.id === 1) {
-    return (
-      <Container id={props.image.name} style={{width: "100vw"}}>
-        {props.image.id}
-          <Title>Search though time</Title>
-          <Gallery image={props.image}/>
-          <Description description={props.image.description} />
-      </Container>
-    )
-  } else if (props.image.title === "") {
-    return (
-      <Container id={props.image.name} style={{ width: "30vw"}} >
-        {props.image.id}
-          <Description description={props.image.description} />
-      </Container>
-    )
+  const slideChoice = useSelector((state) => state.slides.value);
+
+  if (slideChoice === props.image.id) {
+      if (props.image.id === 1) {
+        return (
+          <Container id={props.image.name} style={{width: "100vw"}}>
+            {props.image.id}
+              <Title>Search though time</Title>
+              <Gallery image={props.image}/>
+              <Description description={props.image.description} />
+          </Container>
+        )
+      } else if (props.image.title === "") {
+        return (
+          <Container id={props.image.name} style={{ width: "30vw"}} >
+            {props.image.id}
+              <Description description={props.image.description} />
+          </Container>
+        )
+      } else {
+        return (
+          <Container id={props.image.id} style={{width: "60vw"}}>
+            {props.image.name}
+              <Gallery image={props.image}/>
+              <Description description={props.image.description} />
+          </Container>
+        )
+      }
   } else {
-    return (
-      <Container id={props.image.id} style={{width: "60vw"}}>
-        {props.image.name}
-          <Gallery image={props.image}/>
-          <Description description={props.image.description} />
-      </Container>
-    )
+    return "Hello World";
   }
+
 
 }
 
