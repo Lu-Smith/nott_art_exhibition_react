082010af1f614548dfa6f527ba9c4cfb6e8d46f3
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-01-17 13:15:51 +0000

    Updated ScrollPicture.styled.js

--- src/components/styles/ScrollPicture.styled.js
+++ src/components/styles/ScrollPicture.styled.js
@@ -2,9 +2,14 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
 width: 100%;
+display: flex;
+align-items: center;
+justify-content: center;
 `
 
 export const Page = styled.button`
-font-size: 20px;
+font-size: 18px;
 color: grey;
+padding: 2px 8px;
+border: none;
 `
